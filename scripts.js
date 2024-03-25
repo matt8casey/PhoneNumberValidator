3ee82d104752fc8b199e991e85ff659d1baf4956
Matthew Casey <matt12qb@outlook.com>
2024-03-24 23:11:39 -0400

    finished product

--- scripts.js
+++ scripts.js
@@ -1,15 +1,41 @@
 let userInput = document.getElementById('user-input');
 const clearBtn = document.getElementById('clear-btn');
 const checkBtn = document.getElementById('check-btn');
-const results1 = document.getElementById('return1');
-const results2 = document.getElementById('return2');
-const results3 = document.getElementById('return3');
-let array = [];
+const p = document.getElementsByClassName('return');
+const div = document.getElementById('results-div');
+let i = 0;
 
 const checkIfValid = input => {
-    if (input === '') {
-        alert('Please provide a phone number');
-    }
+  if (input === '') {
+      alert('Please provide a phone number');
+      return
+  }
+  if (i > 2) {
+    i = 0;
+  }
+  if (div.textContent == '') {
+    div.innerHTML = `<p class="return" id="return1"></p>
+    <p class="return" id="return2"></p>
+    <p class="return" id="return3"></p>`
+  }
+  const results1 = document.getElementById('return1');
+  const results2 = document.getElementById('return2');
+  const results3 = document.getElementById('return3');
+  const results = [results1, results2, results3];
+  const countryCode = '^(1\\s?)?';
+  const areaCode = '(\\([0-9]{3}\\)|[0-9]{3})';
+  const spacesDashes = '[\\s\\-]?';
+  const phoneNumber = '[0-9]{3}[\\s\\-]?[0-9]{4}$';
+  const phoneRegex = new RegExp(
+    `${countryCode}${areaCode}${spacesDashes}${phoneNumber}`
+  )
+  if (phoneRegex.test(input)){
+    results[i].innerHTML = `Valid US number: ${userInput.value}`;
+  }
+  else {
+    results[i].innerHTML = `Invalid US number: ${userInput.value}`;
+  }
+  i += 1;
 }
 
 
@@ -25,10 +51,6 @@ document.addEventListener('keydown', e => {
     }
   })
 
-  const countryCode = '^(1\\s)?';
-  const areaCode = '(\\([0-9]{3}\\)|[0-9]{3})';
-  const spacesDashes = '[\\s\\-]?';
-  const phoneNumber = '[0-9]{3}[\\s\\-]?[0-9]{4}$';
-  const phoneRegex = new RegExp(
-    `${countryCode}${areaCode}${spacesDashes}${phoneNumber}`
-  )
+clearBtn.addEventListener('click', () => {
+  div.textContent = '';
+})
